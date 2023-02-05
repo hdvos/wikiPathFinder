e635530f96b79f4e9e75192c31b32129924bdf38
hdvos <hdvos93@gmail.com>
2023-02-05 10:58:43 +0100

    document preprocess text

--- functions/nlpPreprocessing.py
+++ functions/nlpPreprocessing.py
@@ -8,13 +8,26 @@ myStopwords = stopwords.words("english")
 
 
 def preprocess(text: str) -> str:
+    """ Preprocesses the text by removing numbers, stopwords, punctuation and extra spaces.
 
+    :param text: Text to be preprocessed
+    :type text: str
+    :return: Preprocessed text
+    :rtype: str
+    """    
+
+    # Remove all numbers
     text = re.sub(r'[0123456789]+', ' ', text)
+    # Word tokenization
     words = word_tokenize(text)
+    # Remove stopwords
     words = [word.lower()
              for word in words if not word.lower() in myStopwords]
 
+    # Remove punctuation
     text = ' '.join(words)
     text = text.translate(str.maketrans(' ', ' ', string.punctuation))
+    
+    # Remove extra spaces
     text = re.sub(r' +', ' ', text)
     return text
